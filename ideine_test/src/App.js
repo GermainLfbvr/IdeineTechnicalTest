fb83c53ec4bdb330f62aabaa12b11061cd1c4c38
Germain <germain.lefebvre@epitech.eu>
2020-01-16 18:49:54 +0100

    adding treeview of json

--- ideine_test/src/App.js
+++ ideine_test/src/App.js
@@ -1,26 +1,48 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import TreeView from '@material-ui/lab/TreeView';
+import ExpandMoreIcon from '@material-ui/icons/ExpandMore';
+import ChevronRightIcon from '@material-ui/icons/ChevronRight';
+import TreeItem from '@material-ui/lab/TreeItem';
+
+import {makeStyles} from '@material-ui/core/styles';
+
+
+const useStyles = makeStyles(theme => ({
+    root: {
+        height: 216,
+        flexGrow: 1,
+        maxWidth: 400,
+    },
+}));
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+    let data = require('./data/Sectorisation');
+    const classes = useStyles();
+    let tree = build_tree(data.data.roots);
+
+    return (
+        <TreeView
+            className={classes.root}
+            defaultCollapseIcon={<ExpandMoreIcon/>}
+            defaultExpandIcon={<ChevronRightIcon/>}
         >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+            {tree}
+        </TreeView>
+    );
+}
+
+function build_tree(data) {
+    let tree = [];
+    let subTree = [];
+
+    for (let i = 0; i < data.length; i++) {
+      subTree = build_tree(data[i].children);
+      tree.push(
+          <TreeItem nodeId={data[i].id} label={data[i].name}>{subTree}</TreeItem>
+      )
+    }
+    console.log(subTree);
+    return (tree);
 }
 
 export default App;
